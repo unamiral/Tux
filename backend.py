5bb1fce26ccbcb13f8bed9bbd0095f33ef214018
Amirkasra Esmaeilian <amirkasraesmaeilian@gmail.com>
2021-11-03 01:57:27 +0000

    add kick,ban,unban,8ball

--- backend.py
+++ backend.py
@@ -1,4 +1,4 @@
-import requests, json
+import requests, json, random
 from replit import db
 from mlconjug3 import Conjugator
 
@@ -32,3 +32,16 @@ def conjugation(verbe):
     res = verb.conjug_info['Indicatif']
     # 'Présent', 'Imparfait', 'Futur', 'Passé Simple'
     return f"**Présent** => Je **{res['Présent']['1s']}** , Tu **{res['Présent']['2s']}** , Il/Elle/On **{res['Présent']['3s']}** , Nous **{res['Présent']['1p']}** , Vous **{res['Présent']['2p']}** , Ils/Elles **{res['Présent']['3p']}**\n**Imparfait** => Je **{res['Imparfait']['1s']}** , Tu **{res['Imparfait']['2s']}** , Il/Elle/On **{res['Imparfait']['3s']}** , Nous **{res['Imparfait']['1p']}** , Vous **{res['Imparfait']['2p']}** , Ils/Elles **{res['Imparfait']['3p']}**\n**Futur** => Je **{res['Futur']['1s']}** , Tu **{res['Futur']['2s']}** , Il/Elle/On **{res['Futur']['3s']}** , Nous **{res['Futur']['1p']}** , Vous **{res['Futur']['2p']}** , Ils/Elles **{res['Futur']['3p']}**\n**Passé Simple** => Je **{res['Passé Simple']['1s']}** , Tu **{res['Passé Simple']['2s']}** , Il/Elle/On **{res['Passé Simple']['3s']}** , Nous **{res['Passé Simple']['1p']}** , Vous **{res['Passé Simple']['2p']}** , Ils/Elles **{res['Passé Simple']['3p']}**\n> made with mlconjug3"
+
+
+def _8ball():
+    choices = [
+        "It is certain.", "It is decidedly so.", "Without a doubt.",
+        "Yes - definitely.", "You may rely on it.", "As I see it, yes.",
+        "Most likely.", "Outlook good.", "Yes.", "Signs point to yes.",
+        "Reply hazy, try again.", "Ask again later.",
+        "Better not tell you now.", "Cannot predict now.",
+        "Concentrate and ask again.", "Don't count on it.", "My reply is no.",
+        "My sources say no.", "Outlook not so good.", "Very doubtful."
+    ]
+    return random.choice(choices)
--- main.py
+++ main.py
@@ -6,20 +6,58 @@ from discord.ext import commands
 
 bot = commands.Bot(command_prefix="?")
 
+
 @bot.event
 async def on_ready():
     print(f"\a Bot have logged in as {bot.user}")
     await bot.change_presence(activity=discord.Game(name="?h"))
 
+
 @bot.command()
 async def h(ctx):
-  f = open("help.txt")
-  await ctx.send(f.read())
-  f.close()
+    f = open("help.txt")
+    await ctx.send(f.read())
+    f.close()
+
 
 @bot.command()
 async def ping(ctx):
-  await ctx.send(f"Pong! {round(bot.latency * 1000)}ms")
+    await ctx.send(f"Pong! {round(bot.latency * 1000)}ms")
 
+
+@bot.command(aliases=["8ball"])
+async def eightball(ctx, *, qn):
+    await ctx.send(f"Question : {qn}\nAnswer : {backend._8ball()}")
+
+
+@bot.command()
+async def clear(ctx, amount=10):
+    await ctx.channel.purge(limit=amount)
+
+
+@bot.command()
+@commands.has_permissions(administrator=True)
+async def kick(ctx, member: discord.Member, *, reason=None):
+    await member.kick(reason=reason)
+    await ctx.send(f"Kicked : {member.mention}")
+
+
+@bot.command()
+@commands.has_permissions(administrator=True)
+async def ban(ctx, member: discord.Member, *, reason=None):
+    await member.ban(reason=reason)
+    await ctx.send(f"banned : {member.mention}")
+
+@bot.command()
+@commands.has_permissions(administrator=True)
+async def unban(ctx, *, member):
+  bans = await ctx.guild.bans()
+  member_name, member_discriminator = member.split("#")
+  for ban_entry in bans:
+    user = ban_entry.user
+    if (user.name, user.discriminator) == (member_name, member_discriminator):
+      await ctx.guild.unban(user)
+      await ctx.send(f"Unbanned : {user.mention}")
+      return
 keep_alive()
 bot.run(os.getenv("TOKEN"))
