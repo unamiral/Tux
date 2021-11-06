ebd4b6da836833620cfcd8417a236e32475fc866
Amirkasra Esmaeilian <amirkasraesmaeilian@gmail.com>
2021-11-06 01:54:43 +0000

    handle error

--- cogs/study.py
+++ cogs/study.py
@@ -12,7 +12,6 @@ class Study(commands.Cog):
         cg = Conjugator(language="fr")
         verb = cg.conjugate(verbe)
         res = verb.conjug_info['Indicatif']
-        # 'Présent', 'Imparfait', 'Futur', 'Passé Simple'
         embed = discord.Embed(
         title=f"Conjugaison",
         description="A tool for conjugate verbs in french.",
@@ -20,7 +19,7 @@ class Study(commands.Cog):
         embed.set_thumbnail(
         url=
         "https://media.wired.com/photos/59fccff22d3f5732c7d5aa15/master/w_2560%2Cc_limit/Pong-TA-B1C1YX.jpg")
-        embed.add_field(name="Présent", value=f"Je **{res['Présent']['1s']}**,  Tu **{res['Présent']['2s']}**,  Il/Elle/On **{res['Présent']['3s']}**,  Nous **{res['Présent']['1p']}**,  Vous **{res['Présent']['2p']}**,  Ils/Elles **{res['Présent']['3p']}**", inline=False)
+        embed.add_field(name="Présent", value=f"Je **{res['Présent']['1s']}**\n  Tu **{res['Présent']['2s']}**\n  Il/Elle/On **{res['Présent']['3s']}**\n  Nous **{res['Présent']['1p']}**\n  Vous **{res['Présent']['2p']}**\n  Ils/Elles **{res['Présent']['3p']}**\n")
         embed.add_field(name="Imparfait", value=f"Je **{res['Imparfait']['1s']}**\n Tu **{res['Imparfait']['2s']}**\n Il/Elle/On **{res['Imparfait']['3s']}**\n Nous **{res['Imparfait']['1p']}**\n Vous **{res['Imparfait']['2p']}** \n Ils/Elles **{res['Imparfait']['3p']}**")
         embed.add_field(name="Futur", value=f"Je **{res['Futur']['1s']}**\n Tu **{res['Futur']['2s']}**\n Il/Elle/On **{res['Futur']['3s']}**\n Nous **{res['Futur']['1p']}**\n Vous **{res['Futur']['2p']}**\n Ils/Elles **{res['Futur']['3p']}**")
         embed.add_field(name="Passé Simple", value=f"Je **{res['Passé Simple']['1s']}**\n Tu **{res['Passé Simple']['2s']}**\n Il/Elle/On **{res['Passé Simple']['3s']}**\n Nous **{res['Passé Simple']['1p']}**\n Vous **{res['Passé Simple']['2p']}**\n Ils/Elles **{res['Passé Simple']['3p']}**")
@@ -31,11 +30,19 @@ class Study(commands.Cog):
 
     @commands.command()
     async def pi(self, ctx):
-      await ctx.reply(math.pi)
+      embed = discord.Embed(title=f"Pi is {math.pi}", color=0x109319)
+      embed.set_footer(text="The number π (/paɪ/; spelled out as \"pi\") is a mathematical constant, approximately equal to 3.14159. It is defined in Euclidean geometry[a] as the ratio of a circle's circumference to its diameter, and also has various equivalent definitions. The number appears in many formulas in all areas of mathematics and physics. The earliest known use of the Greek letter π to represent the ratio of a circle's circumference to its diameter was by Welsh mathematician William Jones in 1706.[1]It is also referred to as Archimedes's constant. wikipedia")
+      embed.set_thumbnail(url="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2e/Pi-symbol.svg/1058px-Pi-symbol.svg.png")
+      embed.set_author(name="Math", icon_url="https://upload.wikimedia.org/wikipedia/commons/7/7b/Math.png")
+      await ctx.send(embed=embed)
 
     @commands.command()
     async def euler(self, ctx):
-      await ctx.reply(math.e)
+      embed = discord.Embed(title=f"e is {math.e}", color=0x109319)
+      embed.set_footer(text="The number e, also known as Euler's number, is a mathematical constant approximately equal to 2.71828, and can be characterized in many ways. It is the base of the natural logarithm. It is the limit of ⁿ as n approaches infinity, an expression that arises in the study of compound interest. wikipedia")
+      embed.set_thumbnail(url="https://i.pinimg.com/originals/cb/d9/cf/cbd9cf526ee707aafc7a51488c7c2c49.png")
+      embed.set_author(name="Math", icon_url="https://upload.wikimedia.org/wikipedia/commons/7/7b/Math.png")
+      await ctx.send(embed=embed)
 
 def setup(bot):
     bot.add_cog(Study(bot))
--- main.py
+++ main.py
@@ -13,6 +13,29 @@ async def on_ready():
                               activity=discord.Game(name="?help"))
 
 
+@bot.event
+async def on_command_error(ctx, error):
+    if isinstance(error, commands.errors.CommandNotFound):
+        embed = discord.Embed(title=":x: Command Not Founded", color=0xc40906)
+        await ctx.send(embed=embed)
+    elif isinstance(error, commands.errors.MissingRequiredArgument):
+        embed = discord.Embed(
+            title=":x: Please Pass In All Of The Missing Required Argument",
+            color=0xc40906)
+        await ctx.send(embed=embed)
+    elif isinstance(error, commands.errors.MissingPerrmissions):
+        embed = discord.Embed(
+            title=
+            ":x: Sorry! But You Don't Have Enough Permissions To Run This Command",
+            color=0xc40906)
+        await ctx.send(embed=embed)
+    elif isinstance(error, commands.CommandError):
+        embed = discord.Embed(
+            title=":x: Access Denied. This command is only for developer",
+            color=0xc40906)
+        await ctx.send(embed=embed)
+
+
 @bot.command()
 async def ping(ctx):
     embed = discord.Embed(
@@ -26,53 +49,46 @@ async def ping(ctx):
     await ctx.send(embed=embed)
 
 
+def is_me(ctx):
+    return ctx.author.id == 829306875076935680
+
+
 @bot.command()
+@commands.check(is_me)
 async def load(ctx, ext):
-    if ctx.author.id == 829306875076935680:
-        bot.load_extension(f"cogs.{ext}")
-        embed = discord.Embed(title=f"{ext} has loaded.",
-                              description="For load cogs",
-                              color=0x109319)
-        embed.set_thumbnail(
-            url=
-            "https://static1.smartbear.co/smartbear/media/blog/wp/heavy-load.jpg"
-        )
-        await ctx.send(embed=embed)
-    else:
-        await ctx.send(":x: Access Denied. This command is only for developer")
+    bot.load_extension(f"cogs.{ext}")
+    embed = discord.Embed(title=f"{ext} has loaded.",
+                          description="For load cogs",
+                          color=0x109319)
+    embed.set_thumbnail(
+        url=
+        "https://static1.smartbear.co/smartbear/media/blog/wp/heavy-load.jpg")
+    await ctx.send(embed=embed)
 
 
 @bot.command()
+@commands.check(is_me)
 async def unload(ctx, ext):
-    if ctx.author.id == 829306875076935680:
-        bot.unload_extension(f"cogs.{ext}")
-        embed = discord.Embed(title=f"{ext} has unloaded.",
-                              description="For unload cogs",
-                              color=0xc40906)
-        embed.set_thumbnail(
-            url=
-            "https://i.imgur.com/05De7kW.png"
-        )
-        await ctx.send(embed=embed)
-    else:
-        await ctx.send(":x: Access Denied. This command is only for developer")
+    bot.unload_extension(f"cogs.{ext}")
+    embed = discord.Embed(title=f"{ext} has unloaded.",
+                          description="For unload cogs",
+                          color=0xc40906)
+    embed.set_thumbnail(url="https://i.imgur.com/05De7kW.png")
+    await ctx.send(embed=embed)
 
 
 @bot.command()
+@commands.check(is_me)
 async def reload(ctx, ext):
-    if ctx.author.id == 829306875076935680:
-        bot.unload_extension(f"cogs.{ext}")
-        bot.load_extension(f"cogs.{ext}")
-        embed = discord.Embed(title=f"{ext} has reloaded.",
-                              description="For reload cogs",
-                              color=0x109319)
-        embed.set_thumbnail(
-            url=
-            "https://cdn2.iconfinder.com/data/icons/ios-7-icons/50/reload-512.png"
-        )
-        await ctx.send(embed=embed)
-    else:
-        await ctx.send(":x: Access Denied. This command is only for developer")
+    bot.unload_extension(f"cogs.{ext}")
+    bot.load_extension(f"cogs.{ext}")
+    embed = discord.Embed(title=f"{ext} has reloaded.",
+                          description="For reload cogs",
+                          color=0x109319)
+    embed.set_thumbnail(
+        url=
+        "https://cdn2.iconfinder.com/data/icons/ios-7-icons/50/reload-512.png")
+    await ctx.send(embed=embed)
 
 
 for file in os.listdir("./cogs"):
@@ -80,6 +96,7 @@ for file in os.listdir("./cogs"):
         bot.load_extension(f"cogs.{file[:-3]}")
 
 # @bot.command()
+# @commands.has_permissions(manage_messages=True)
 # async def clear(ctx, amount=10):
 #     await ctx.channel.purge(limit=amount)
 
