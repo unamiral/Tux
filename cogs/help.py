1091f191f6d621dcb0b43b7c0fe2f233eebae295
amirkasraa <amirkasra.esmaeilian@gmail.com>
2021-11-06 10:39:04 -0400

    add custom help message

---
+++ cogs/help.py
@@ -0,0 +1,122 @@
+import discord
+from discord.ext import commands
+from discord.errors import Forbidden
+
+
+async def send_embed(ctx, embed):
+    """
+    Function that handles the sending of embeds
+    -> Takes context and embed to send
+    - tries to send embed in channel
+    - tries to send normal message when that fails
+    - tries to send embed private with information abot missing permissions
+    """
+    try:
+        await ctx.send(embed=embed)
+    except Forbidden:
+        try:
+            await ctx.send("Hey, seems like I can't send embeds. Please check my permissions :)")
+        except Forbidden:
+            await ctx.author.send(
+                f"Hey, seems like I can't send any message in {ctx.channel.name} on {ctx.guild.name}\n"
+                f"May you inform the server team about this issue? :slight_smile: ", embed=embed)
+
+
+class Help(commands.Cog):
+    """
+    Sends this help message
+    """
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def help(self, ctx, *input):
+        """Shows all modules of that bot"""
+
+        prefix = "?"
+        version = "1.0"
+
+        # setting owner name - if you don't wanna be mentioned remove line 49-60 and adjust help text (line 88)
+        owner = 829306875076935680
+        owner_name = "amirkasraa#8702"
+
+        if not input:
+            # checks if owner is on this server - used to 'tag' owner
+            try:
+                owner = ctx.guild.get_member(owner).mention
+
+            except AttributeError as e:
+                owner = owner
+
+            # starting to build embed
+            emb = discord.Embed(
+                title='Commands and modules', color=0x2b75a6,
+                description=f'Use `{prefix}help <module>` to gain more information about that module\n :smile:\nFor more information, You can watch this video : https://youtu.be/dQw4w9WgXcQ')
+
+            # iterating trough cogs, gathering descriptions
+            cogs_desc = ''
+            for cog in self.bot.cogs:
+                cogs_desc += f'`{cog}` {self.bot.cogs[cog].__doc__}\n'
+
+            # adding 'list' of cogs to embed
+            emb.add_field(name='Modules', value=cogs_desc, inline=False)
+
+            # integrating trough uncategorized commands
+            commands_desc = ''
+            for command in self.bot.walk_commands():
+                # if cog not in a cog
+                # listing command if cog name is None and command isn't hidden
+                if not command.cog_name and not command.hidden:
+                    commands_desc += f'{command.name} - {command.help}\n'
+
+            # adding those commands to embed
+            if commands_desc:
+                emb.add_field(name='Not belonging to a module', value=commands_desc, inline=False)
+
+            # setting information about author
+            emb.add_field(name="About", value=f"The Bots is developed by amirkasraa#8702, based on discord.py.\n\
+                                    This version of it is maintained by {owner}\n\
+                                    Please visit https://github.com/amirkasraa/Tux to submit ideas or bugs.")
+            emb.set_footer(text=f"Bot is running {version}")
+
+        # block called when one cog-name is given
+        # trying to find matching cog and it's commands
+        elif len(input) == 1:
+
+            # iterating trough cogs
+            for cog in self.bot.cogs:
+                # check if cog is the matching one
+                if cog.lower() == input[0].lower():
+
+                    # making title - getting description from doc-string below class
+                    emb = discord.Embed(title=f'{cog} - Commands', description=self.bot.cogs[cog].__doc__,
+                                        color=0x109319)
+
+                    # getting commands from cog
+                    for command in self.bot.get_cog(cog).get_commands():
+                        # if cog is not hidden
+                        if not command.hidden:
+                            emb.add_field(name=f"`{prefix}{command.name}`", value=command.help, inline=False)
+                    # found cog - breaking loop
+                    break
+
+            # if input not found
+            # yes, for-loops have an else statement, it's called when no 'break' was issued
+            else:
+                emb = discord.Embed(title="What's that?!",
+                                    description=f"I've never heard from a module called `{input[0]}` before :scream:",
+                                    color=0xfc5a03)
+
+        # too many cogs requested - only one at a time allowed
+        elif len(input) > 1:
+            emb = discord.Embed(title="That's too much.",
+                                description="Please request only one module at once :sweat_smile:",
+                                color=0xfc5a03)
+
+        # sending reply embed using our own function defined above
+        await send_embed(ctx, emb)
+
+
+def setup(bot):
+    bot.add_cog(Help(bot))
--- main.py
+++ main.py
@@ -5,24 +5,9 @@ from ping import keep_alive
 from discord.ext import commands
 
 
-class CustomHelpCommand(commands.HelpCommand):
-    def __init__(self):
-        super().__init__()
-
-    async def send_cog_help(self, cog):
-        return await super().send_cog_help(cog)
-
-    async def send_bot_help(self, mapping):
-        return await super().send_bot_help(mapping)
-
-    async def send_command_help(self, command):
-        return await super().send_command_help(command)
-
-    async def send_group_help(self, group):
-        return await super().send_group_help(group)
 
 
-bot = commands.Bot(command_prefix="?")
+bot = commands.Bot(command_prefix="?", help_command=Help())
 
 
 @bot.event
